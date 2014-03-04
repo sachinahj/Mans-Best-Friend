ccdb0aceb72172c82d00bc18b7bb075c7908f829
Sachin Ahuja <Sachin@sachin-ahujas-macbook-pro.local>
2014-03-03 22:31:46 -0600

    Initial Commit

---
+++ Man's Best Friend/SAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SAAppDelegate.h
+//  Man's Best Friend
+//
+//  Created by Sachin Ahuja on 3/3/14.
+//  Copyright (c) 2014 Sachin Ahuja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Man's Best Friend/SAViewController.h
@@ -0,0 +1,13 @@
+//
+//  SAViewController.h
+//  Man's Best Friend
+//
+//  Created by Sachin Ahuja on 3/3/14.
+//  Copyright (c) 2014 Sachin Ahuja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAViewController : UIViewController
+
+@end
